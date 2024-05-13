09c4850392c48578d5ace4033ebf35f8071cd3e3
deepakfenner <143514088+deepakfenner@users.noreply.github.com>
2024-05-13 22:10:54 +0530

    Add files via upload

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,8 +1,16 @@
 const express=require('express')
 const router=express.Router()
 
-router.get('/',(req,res)=>{
-    res.render('index')
+router.get('',(req,res)=>{
+    const locals={
+        title:"nodejsblog",
+        description:"hello world"
+    }
+    res.render('index',{locals})
+});
+
+router.get('/about',(req,res)=>{
+    res.render('about')
 })
 
 module.exports=router
