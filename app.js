02b1f3cc48e2a6f0eb5d42f4541b8b454438cdac
deepakfenner <143514088+deepakfenner@users.noreply.github.com>
2024-05-13 22:09:39 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const express=require('express')
 const expressLayout=require('express-ejs-layouts')
 const app=express()
 const PORT= 5000 || process.env.PORT
-app.use('/',require('./server/routes/main'))
+
 app.use(express.static('public'))
 app.use(expressLayout)
 app.set('layout','./layouts/main')
 app.set('view engine','ejs')
 
 
-
+app.use('/',require('./server/routes/main'))
 app.listen(PORT);
 
